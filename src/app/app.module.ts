9076690ea6345470112496909c094de6c7f7463d
Sugeng Warsito <sugengwarsito131001@gmail.com>
2023-08-27 09:41:59 +0700

    feat: add sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeadbarComponent } from './headbar/headbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HeadbarComponent
+    HeadbarComponent,
+    SidebarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+  standalone: true,
+  imports: [ MatButtonModule, MatIconModule, MatSidenavModule]
+})
+export class SidebarComponent {
+
+}
